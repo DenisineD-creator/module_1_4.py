8363c689897e38d84ee096912aea3898a3842625
DenisineD-creator <dburobin1@mail.ru>
2024-08-27 20:12:53 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,12 @@
+
+my_string = str(input('доброе утро'))
+print(len('доброе утро'))
+print('доброе утро'.upper())
+print('доброе утро'.lower())
+#print('доброе утро'.replace(_old:' ',_new:''))
+print('доброе утро'[0])
+print('доброе утро'[-1])
+
+
+
+
